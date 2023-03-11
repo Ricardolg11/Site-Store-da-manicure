79343f302adb6385ebcc3f65126f26b9339653f7
ricardo lg <solop162@gmail.com>
2023-03-11 20:29:41 -0300

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,92 @@
+/*------------ SHOW MENU-------------*/
+const showMneu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    /*valida se se existe as variaveis*/
+    if (toggle && nav) {
+        /*aqui adiciono a class show-menu na div com a class nav__menu*/
+        toggle.addEventListener('click', () => {
+            /*adicina a class show-menu na div que tem o id nav-menu */
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMneu('nav-toggle', 'nav')
+
+/*------------ REMOVER MENU MOBILE-------------*/
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction() {
+    const navMenu = document.getElementById('nav')
+    /*Quando um link for clicado a class show-menu será removido*/
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== DEIXA O LINK CLICADO COM A CLASS ACTIVE-LINK ====================*/
+const linkColor = document.querySelectorAll('.nav__item')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(L => L.classList.remove('active-link'))
+        this.classList.add('active-link')
+    }
+}
+
+linkColor.forEach(L=> L.addEventListener('click', colorLink))
+
+/** SWIPER */
+var swiper = new Swiper("#hero", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }
+});
+
+var swiper = new Swiper("#offer", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation:{
+    nextEL: ".swiper-button-next",
+    prevEL: ".swiper-button-prev",
+  },
+  breakpoints: {
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    860: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    960: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
+var swiper = new Swiper("#inspiration-img", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 0,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
